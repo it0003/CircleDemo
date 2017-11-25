900f00990c8e226276ef0ebde2fc7853214d483c
Chen Meng <qrmail@qingruikeji.cn>
2017-11-25 18:02:51 +0800

    提交成功！！！

--- app/src/main/java/com/yiw/circledemo/widgets/SnsPopupWindow.java
+++ app/src/main/java/com/yiw/circledemo/widgets/SnsPopupWindow.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yiw.circledemo.R;
 import com.yiw.circledemo.bean.ActionItem;
